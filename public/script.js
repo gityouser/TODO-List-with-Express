5759d687fc66c9e4fc2eb02d5ce05f0b71196861
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2018-02-06 08:16:50 +0200

    Added element render and delete

--- public/script.js
+++ public/script.js
@@ -1,14 +1,29 @@
 const input = document.querySelector('input');
 const itemList = document.querySelector('.ul-list');
 const form = document.querySelector('.form');
+const clearList = document.querySelector('.clear-list');
 const inputList = [];
 
 
 function initiateListeners() {
   form.addEventListener('submit', pushToInputList);
-
+  clearList.addEventListener('click', clearEntireList)
 }
 initiateListeners();
+
+function pushToInputList(e) {
+  e.preventDefault();
+  if(input.value.length > 0) {
+    inputList.push({
+      name:input.value,
+      completed:false
+    });
+    input.value = '';
+    renderListItems();
+  } else {
+    alert("You must type something.")
+  }
+}
 
 function renderListItems() {
   const li = document.createElement('li');
@@ -17,19 +32,30 @@ function renderListItems() {
     itemList.appendChild(li);
     li.innerText = item.name;
     li.appendChild(img);
-    li.style.cssText = ''
-    img.src = "https://cdn.pixabay.com/photo/2012/04/02/16/12/x-24850_960_720.png";
+    li.classList.add('li');
+    img.src = "http://panchkula.nic.in/wp-content/uploads/2017/06/x-mark.png";
     img.classList.add('x-mark');
+    li.addEventListener('click', markAsCompleted);
+    img.addEventListener('click', deleteListItems);
   })
-    img.addEventListener('click', () => console.log("XXXXXXxxMark!!!"));
 }
 
-function pushToInputList(e) {
-  e.preventDefault();
-  inputList.push({
-    name:input.value,
-    completed:false
-  });
-  input.value = '';
-  renderListItems();
+function markAsCompleted(event) {
+  if(event.target.completed === false) {
+    event.target.style.cssText = 'color:grey; text-decoration: line-through'
+    event.target.completed = true;
+  } else {
+    event.target.completed = false;
+  }
+
+}
+
+function deleteListItems(event) {
+  event.target.parentElement.remove();
+}
+function clearEntireList() {
+  while(itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+    // itemList.firstChild.remove();
+  }
 }
