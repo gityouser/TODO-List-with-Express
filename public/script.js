628499a430e5735819edaf4bd7304fb9b6ec4a1d
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2018-02-09 00:55:55 +0200

    Lots of bugs squashed.

--- public/script.js
+++ public/script.js
@@ -21,11 +21,12 @@ function pushToInputList(e) {
     input.value = '';
     renderListItems();
   } else {
-    alert("You must type something.")
+    alert("Add data to the list.")
   }
 }
 
 function renderListItems() {
+//QQ
   for (let i = ul.children.length; i > 0; i--) {
     ul.removeChild(ul.children[i-1])
   }
@@ -38,27 +39,35 @@ function renderListItems() {
     li.classList.add('li');
     img.src = "http://panchkula.nic.in/wp-content/uploads/2017/06/x-mark.png";
     img.classList.add('x-mark');
-    li.addEventListener('click', markAsCompleted);
+//Both of the below indexes will be mapped by forEach to each node element at the moment the function run and WILL NOT synchronize automatically with the inputList array indexes when nodes get removed!
+    li.addEventListener('click', () => markAsCompleted(index));
     img.addEventListener('click', () => deleteListItems(index))
   });
 }
 
 function deleteListItems(index) {
-    if(index = 1) {
-      ul.removeChild(ul.children[0]);
-    } else {
-      ul.removeChild(ul.children[index]);
-    }
-    inputList.splice(index, 1);
+  console.log(index);
+// Grab the index of the node targeted by the event/click.
+  let nodeIndex = Array.from(ul.children).indexOf(ul.children[index]);
+  console.log(nodeIndex);
+//Check if the idex of the node element equalsh the coresponding index in inputList array.
+  if (Array.from(ul.children).indexOf(ul.children[index]) < index) {
+     ul.removeChild(ul.children[nodeIndex +1])
+  } else {
+    ul.removeChild(ul.children[index]);
+  }
+    inputList.splice(index -1, 1);
     console.log(inputList);
 };
 
-function markAsCompleted(event) {
-  if(event.target.completed === false) {
+function markAsCompleted(index) {
+
+  if(inputList[index].completed === false) {
+    console.log(event); //Why is event 'the event'?
     event.target.style.cssText = 'color:green; text-decoration: line-through'
-    event.target.completed = true;
+    inputList[index].completed = true;
   } else {
-    event.target.completed = false;
+    inputList[index].completed = false;
     event.target.style.cssText = 'color:black; text-decoration: none'
   }
 }
